e1f3a276836208e14e15dc20049aad708c142c3b
xwoud <xwoud@naver.com>
2021-03-13 16:42:03 +0900

    [upload] Font Extension

---
+++ nalul/nalul/Global/Extension/UIFont+Extension.swift
@@ -0,0 +1,57 @@
+//
+//  UIFont+Extension.swift
+//  nalul
+//
+//  Created by 김민희 on 2021/03/13.
+//
+
+import UIKit
+
+extension UIFont {
+    
+    // MARK: 나눔손글씨 딸에게 엄마가 Font
+    class func momFont(size: CGFloat) -> UIFont {
+        return UIFont(name: "나눔손글씨 딸에게 엄마가", size: size)!
+    }
+    
+    // MARK: S-CoreDream Font
+    class func oneThin(size: CGFloat) -> UIFont {
+        return UIFont(name: "S-CoreDream-1Thin", size: size)!
+    }
+    
+    class func twoExLight(size: CGFloat) -> UIFont {
+        return UIFont(name: "S-CoreDream-2ExtraLight", size: size)!
+    }
+    
+    class func threeLight(size: CGFloat) -> UIFont {
+        return UIFont(name: "S-CoreDream-3Light", size: size)!
+    }
+    
+    class func fourRegular(size: CGFloat) -> UIFont {
+        return UIFont(name: "S-CoreDream-4Regular", size: size)!
+    }
+    
+    class func fiveMedium(size: CGFloat) -> UIFont {
+        return UIFont(name: "S-CoreDream-5Medium", size: size)!
+    }
+    
+    class func sixBold(size: CGFloat) -> UIFont {
+        return UIFont(name: "S-CoreDream-6Bold", size: size)!
+    }
+    
+    class func sevenExBold(size: CGFloat) -> UIFont {
+        return UIFont(name: "S-CoreDream-7ExtraBold", size: size)!
+    }
+    
+    class func eightHeavy(size: CGFloat) -> UIFont {
+        return UIFont(name: "S-CoreDream-8Heavy", size: size)!
+    }
+    
+    class func nineBlack(size: CGFloat) -> UIFont {
+        return UIFont(name: "S-CoreDream-9Black", size: size)!
+    }
+    
+    
+}
+
+
