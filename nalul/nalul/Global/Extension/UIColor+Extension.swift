a94ec04145fba6451d9924318d75676fae315c4c
xwoud <xwoud@naver.com>
2021-03-17 18:07:22 +0900

    [upload] UIColor Extension 생성
    body: 컬러칩 추가

---
+++ nalul/nalul/Global/Extension/UIColor+Extension.swift
@@ -0,0 +1,24 @@
+//
+//  UIColor+Extension.swift
+//  nalul
+//
+//  Created by 김민희 on 2021/03/17.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    @nonobjc class var nalulBlack: UIColor {
+        return UIColor(red: 20.0 / 255.0, green: 20.0 / 255.0, blue: 20.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var nalulDarkGray: UIColor {
+        return UIColor(red: 53.0 / 255.0, green: 51.0 / 255.0, blue: 51.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var nalulLightGray: UIColor {
+        return UIColor(red: 96.0 / 255.0, green: 93.0 / 255.0, blue: 93.0 / 255.0, alpha: 1.0)
+    }
+
+}
