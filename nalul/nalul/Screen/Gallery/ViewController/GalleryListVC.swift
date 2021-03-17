49a50e37d94d35c1f28d11455966d7b4cbb760c5
xwoud <xwoud@naver.com>
2021-03-17 16:41:37 +0900

    [style] IBOutlet 설정

--- nalul/nalul/Screen/Gallery/ViewController/GalleryListVC.swift
+++ nalul/nalul/Screen/Gallery/ViewController/GalleryListVC.swift
@@ -9,6 +9,22 @@ import UIKit
 
 class GalleryListVC: UIViewController {
 
+    // MARK: Variable Part
+    
+    // MARK: IBOutlet
+    
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var writeButton: UIButton!
+    
+    @IBOutlet weak var galleryListCollectionView:
+        UICollectionView!
+    
+    // MARK: IBAction
+    
+    // MARK: Life Cycle Part
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +32,17 @@ class GalleryListVC: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+// MARK: Extension
+
+extension GalleryListVC {
+    
+    // MARK: Function
+    func setButton() {
+        
+        writeButton.makeRounded(cornerRadius: nil)
+
     }
-    */
-
+    
 }
