ce3adfb04edb8751363f634f1ffeca6136fb8c30
xwoud <xwoud@naver.com>
2021-03-17 17:01:27 +0900

    [upload] CollectionView Cell 생성
    body: 갤러리 리스트에 대한 Cell 파일 생성
    
    Related to: #13

---
+++ nalul/nalul/Screen/Gallery/Cell/GalleryListCollectionViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GalleryListCollectionViewCell.swift
+//  nalul
+//
+//  Created by 김민희 on 2021/03/17.
+//
+
+import UIKit
+
+class GalleryListCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: Variable Part
+    
+    // MARK: IBOutlet
+    
+    // MARK: Life Cycle Part
+    
+    override func awakeFromNib() {
+         
+    }
+    
+    // MARK: Function
+    
+}
