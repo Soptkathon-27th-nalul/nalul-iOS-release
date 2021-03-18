9225e9368feceeb444d12d2347c072ad45925dc9
xwoud <xwoud@naver.com>
2021-03-18 17:10:22 +0900

    [style] gallery detail view 완료
    body: View, Label, Button, TextView 모두 디자인 완료
    
    Related to: #19

--- nalul/nalul/Screen/Gallery/ViewController/GalleryDetailVC.swift
+++ nalul/nalul/Screen/Gallery/ViewController/GalleryDetailVC.swift
@@ -8,22 +8,109 @@
 import UIKit
 
 class GalleryDetailVC: UIViewController {
-
+    
+    // MARK: Variable Part
+    
+    var titleName: String?
+    var postDate: String?
+    var userPhoto: UIImage?
+    var question: String?
+    var answer: String?
+    
+    // MARK: IBOutlet
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var deleteButton: UIButton!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var userImageView: UIImageView!
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answerTextView: UITextView!
+    
+    @IBOutlet weak var bottomLayoutConstraint: NSLayoutConstraint!
+    
+    // MARK: IBAction
+    
+    
+    // MARK: Life Cycle Part
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setLabel()
+        setView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        setButton()
+        if view.safeAreaInsets.bottom == 0.0 {
+            bottomLayoutConstraint.constant = 20
+        }
     }
-    */
+    
+}
 
+// MARK: Extension
+
+extension GalleryDetailVC {
+    
+    // MARK: Function
+    
+    func setView() {
+        
+        if let userPhoto = userPhoto {
+            userImageView.image = userPhoto
+        } else {
+            userImageView.image = UIImage(named: "miniTest")
+        }
+    }
+    
+    func setLabel() {
+        
+        if let titleName = titleName {
+            titleLabel.text = titleName
+        } else {
+            titleLabel.text = "왼쪽 눈"
+        }
+        
+        titleLabel.font = UIFont.threeLight(size: 17)
+        titleLabel.textColor = .white
+        
+        if let postDate = postDate {
+            dateLabel.text = postDate
+        } else {
+            dateLabel.text = "20210424"
+        }
+        
+        dateLabel.font = UIFont.momFont(size: 16)
+        dateLabel.textColor = .white
+        
+        if let question = question {
+            questionLabel.text = question
+        } else {
+            questionLabel.text = "오늘 당신의 눈은 어떤 색인가요?"
+        }
+        
+        questionLabel.font = UIFont.threeLight(size: 15)
+        questionLabel.textColor = .white
+
+        if let answer = answer {
+            answerTextView.text = answer
+        } else {
+            answerTextView.text = "내 오른쪽 눈은 적색일지도 모른다.\n밤샘이 잦은 탓에,\n눈에 돋은 핏줄을 숨길 수가 없다.\n\n조만간 푹 쉬고\n흰색의 눈에 갈색의 동공이 빛나는\n건강한 하루를 보내고 싶다."
+        }
+        answerTextView.textColor = .white
+        answerTextView.font = UIFont.oneThin(size: 14)
+        answerTextView.isEditable = false
+        answerTextView.lineSetting(kernValue: 0.0, lineSpacing: 8.0)
+    }
+    
+    func setButton() {
+        deleteButton.setTitle("삭제", for: .normal)
+        deleteButton.titleLabel?.font = UIFont.threeLight(size: 14)
+        deleteButton.tintColor = .white
+        deleteButton.setBorder(borderColor: .white, borderWidth: 1)
+        deleteButton.makeRounded(cornerRadius: nil)
+    }
+    
 }
