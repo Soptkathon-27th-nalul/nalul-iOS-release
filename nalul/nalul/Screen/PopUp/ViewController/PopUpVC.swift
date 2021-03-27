4f39b3d61e4b4fdad9dae1641a6671621e60f52a
xwoud <xwoud@naver.com>
2021-03-27 19:06:48 +0900

    [style] 코드 구조 잡기
    body: Mark 문법으로 코드 구조 잡기
    
    Related to: #23

--- nalul/nalul/Screen/PopUp/ViewController/PopUpVC.swift
+++ nalul/nalul/Screen/PopUp/ViewController/PopUpVC.swift
@@ -9,20 +9,24 @@ import UIKit
 
 class PopUpVC: UIViewController {
 
+    // MARK: Variable Part
+    
+    // MARK: IBOutlet
+    
+    // MARK: IBAction
+    
+    // MARK: Life Cycle Part
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+
+
+}
+
+// MARK: Extension
+
+extension PopUpVC {
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
