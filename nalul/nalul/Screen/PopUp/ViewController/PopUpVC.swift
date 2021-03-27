d0919a030bf05ad3827521ddd0ba624803eed931
xwoud <xwoud@naver.com>
2021-03-27 19:04:16 +0900

    [upload] popup View

---
+++ nalul/nalul/Screen/PopUp/ViewController/PopUpVC.swift
@@ -0,0 +1,28 @@
+//
+//  PopUpVC.swift
+//  nalul
+//
+//  Created by 김민희 on 2021/03/27.
+//
+
+import UIKit
+
+class PopUpVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
