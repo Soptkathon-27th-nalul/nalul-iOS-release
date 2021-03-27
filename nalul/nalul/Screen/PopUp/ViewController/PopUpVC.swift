16d13e4928f5bfaa0621c0c21eacefac47e654e7
xwoud <xwoud@naver.com>
2021-03-27 19:22:41 +0900

    [style] label, view 컬러 설정
    body: label과 view에 대한 스타일 지정
    
    Related to: #23

--- nalul/nalul/Screen/PopUp/ViewController/PopUpVC.swift
+++ nalul/nalul/Screen/PopUp/ViewController/PopUpVC.swift
@@ -11,7 +11,15 @@ class PopUpVC: UIViewController {
 
     // MARK: Variable Part
     
+    var questionMent: String?
+    var explainMent: String?
+    
+    
     // MARK: IBOutlet
+    
+    @IBOutlet weak var popUpView: UIView!
+    @IBOutlet weak var mentLabel: UILabel!
+    @IBOutlet weak var buttonView: UIView!
     
     // MARK: IBAction
     
@@ -19,9 +27,10 @@ class PopUpVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setView()
+        setLabel()
         // Do any additional setup after loading the view.
     }
-
 
 }
 
@@ -29,4 +38,50 @@ class PopUpVC: UIViewController {
 
 extension PopUpVC {
     
+    // MARK: View Style Function
+    
+    func setView() {
+        self.view.backgroundColor = .nalulBlack
+        self.view.backgroundColor?.withAlphaComponent(0.3)
+        
+        self.popUpView.backgroundColor = .nalulBlack
+        self.popUpView.backgroundColor?.withAlphaComponent(0.8)
+        self.popUpView.setRounded(radius: 32.5)
+        self.popUpView.setBorder(borderColor: .nalulLightGray, borderWidth: 1)
+        
+        buttonView.backgroundColor = .clear
+    }
+    
+    // MARK: Label Style Function
+    
+    func setLabel() {
+        
+        questionMent = "기록을 삭제하시겠어요?"
+        explainMent = "삭제된 기록은 복구할 수 없습니다."
+        // Test 코드
+        
+        if let question = questionMent,
+           let explain = explainMent {
+            mentLabel.numberOfLines = 0
+            mentLabel.text = "\(question)\n\(explain)"
+            mentLabel.textColor = .white
+            mentLabel.font = .threeLight(size: 14)
+            
+            if let text = mentLabel.text {
+                // 두번째줄만 style을 다르게 설정하기
+                
+                let attributedStr = NSMutableAttributedString(string: text)
+                attributedStr.addAttribute(NSAttributedString.Key(rawValue: kCTFontAttributeName as String), value: UIFont.twoExLight(size: 11), range: (text as NSString).range(of: explain))
+                // 폰트 수정
+                attributedStr.addAttribute(.foregroundColor, value: UIColor(white: 1.0, alpha: 0.6), range: (text as NSString).range(of: explain))
+                // label 컬러에 불투명도 값 주기
+                
+                mentLabel.attributedText = attributedStr
+            }
+            
+            mentLabel.lineSetting(kernValue: 0, lineSpacing: 12)
+        }
+        
+        
+    }
 }
