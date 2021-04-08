2d8fa31b3d6bc554f0c91b95a5b84224085b7d3f
xwoud <xwoud@naver.com>
2021-04-09 00:59:49 +0900

    [sytle] 뷰 스타일 지정
    body: 뷰 디자인 짜기
    
    Related to: #26

--- nalul/nalul/Screen/Gallery/ViewController/PhotoInsertVC.swift
+++ nalul/nalul/Screen/Gallery/ViewController/PhotoInsertVC.swift
@@ -11,7 +11,18 @@ class PhotoInsertVC: UIViewController {
 
     // MARK: Variable Part
     
+    var titleName: String?
+    var postDate: String?
+    var question: String?
+    
     // MARK: IBOutlet
+    
+    @IBOutlet weak var insertButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var postDateLabel: UILabel!
+    @IBOutlet weak var photoChoseButton: UIButton!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answerTextField: UITextField!
     
     // MARK: IBAction
     
@@ -19,8 +30,12 @@ class PhotoInsertVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setLabel()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidLayoutSubviews() {
+        setButton()
     }
     
 }
@@ -30,5 +45,50 @@ class PhotoInsertVC: UIViewController {
 extension PhotoInsertVC {
     
     // MARK: Function
+    
+    func setLabel() {
+        if let titleName = titleName {
+            titleLabel.text = titleName
+        } else {
+            titleLabel.text = "왼쪽 눈"
+        }
+        
+        titleLabel.font = UIFont.threeLight(size: 17)
+        titleLabel.textColor = .white
+        
+        if let postDate = postDate {
+            postDateLabel.text = postDate
+        } else {
+            postDateLabel.text = "20210424"
+        }
+        
+        postDateLabel.font = UIFont.momFont(size: 16)
+        postDateLabel.textColor = .white
+        
+        if let question = question {
+            questionLabel.text = question
+        } else {
+            questionLabel.text = "오늘 당신의 눈은 어떤 색인가요?"
+        }
+        
+        questionLabel.font = UIFont.threeLight(size: 15)
+        questionLabel.textColor = .white
+        questionLabel.numberOfLines = 0
+        
+        answerTextField.textColor = .white
+        answerTextField.font = UIFont.oneThin(size: 14)
+        answerTextField.textAlignment = .left
+        
+    }
+    
+    func setButton() {
+        
+        insertButton.setTitle("등록", for: .normal)
+        insertButton.titleLabel?.font = UIFont.threeLight(size: 14)
+        insertButton.tintColor = .white
+        insertButton.setBorder(borderColor: .white, borderWidth: 1)
+        insertButton.makeRounded(cornerRadius: nil)
+    }
+    
     
 }
