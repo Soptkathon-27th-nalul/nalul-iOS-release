719d885c066752fb8c9ec6a2cffa3f0fde5cfac9
xwoud <xwoud@naver.com>
2021-04-09 16:12:15 +0900

    [feat] textview 설정
    body: PlaceHolder 설정 및 키보드 올리고 내릴 때 스크롤 설정 완료
    
    Related to: #26

--- nalul/nalul/Screen/Gallery/ViewController/PhotoInsertVC.swift
+++ nalul/nalul/Screen/Gallery/ViewController/PhotoInsertVC.swift
@@ -21,7 +21,8 @@ class PhotoInsertVC: UIViewController {
     @IBOutlet weak var postDateLabel: UILabel!
     @IBOutlet weak var photoChoseButton: UIButton!
     @IBOutlet weak var questionLabel: UILabel!
-    @IBOutlet weak var answerTextField: UITextField!
+    @IBOutlet weak var answerTextView: UITextView!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     // MARK: IBAction
     
@@ -30,6 +31,9 @@ class PhotoInsertVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setLabel()
+        setView()
+        scrollViewTap()
+        textViewPlaceHolder(textView: answerTextView)
         // Do any additional setup after loading the view.
     }
     
@@ -46,6 +50,7 @@ extension PhotoInsertVC {
     // MARK: Function
     
     func setLabel() {
+        
         if let titleName = titleName {
             titleLabel.text = titleName
         } else {
@@ -70,10 +75,21 @@ extension PhotoInsertVC {
         questionLabel.textColor = .white
         questionLabel.numberOfLines = 0
         
-        answerTextField.textColor = .white
-        answerTextField.font = UIFont.oneThin(size: 14)
-        answerTextField.textAlignment = .left
+        answerTextView.textColor = .white
+        answerTextView.font = UIFont.oneThin(size: 14)
+        answerTextView.textAlignment = .left
+        answerTextView.backgroundColor = .none
         
+    }
+    
+    func setView() {
+        
+        answerTextView.delegate = self
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(touchTextView), name: UIResponder.keyboardWillShowNotification, object: nil)
+        // 키보드가 화면에 띄어질 때 소환
+        NotificationCenter.default.addObserver(self, selector: #selector(touchDownView), name: UIResponder.keyboardWillHideNotification, object: nil)
+        // 키보드가 화면에서 내려가면 소환
     }
     
     func setButton() {
@@ -85,5 +101,60 @@ extension PhotoInsertVC {
         insertButton.makeRounded(cornerRadius: nil)
     }
     
+    @objc func touchTextView(notification: NSNotification) {
+        // 스크롤 위로 올리기
+        
+        scrollView.setContentOffset(CGPoint(x: 0, y: self.answerTextView.frame.height+100), animated: true)
+
+    }
+    
+    @objc func touchDownView() {
+        // 스크롤 다시 내리기
+        
+        scrollView.scrollRectToVisible(CGRect(x: 0, y: 0, width: 1, height: 1), animated: true)
+
+    }
+    
+    func scrollViewTap() {
+        //스크롤뷰 위에서 탭하면 키보드 내리기
+        
+        let singleTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(myTapMethod))
+        
+        singleTapGestureRecognizer.numberOfTapsRequired = 1
+        singleTapGestureRecognizer.isEnabled = true
+        singleTapGestureRecognizer.cancelsTouchesInView = false
+        scrollView.addGestureRecognizer(singleTapGestureRecognizer)
+    }
+    
+    @objc func myTapMethod(sender: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
+    
+    func textViewPlaceHolder(textView: UITextView) {
+        // textView에 PlaceHolder 설정하기
+        
+        if textView.text == "대답을 입력해주세요." {
+            textView.text = ""
+            insertButton.isEnabled = true
+        } else if textView.text == "" {
+            textView.text = "대답을 입력해주세요."
+            insertButton.isEnabled = false
+        }
+    }
+    
+}
+
+// MARK: UITextFieldDelegate
+
+extension PhotoInsertVC: UITextViewDelegate {
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textViewPlaceHolder(textView: textView)
+        textView.becomeFirstResponder()
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        textViewPlaceHolder(textView: textView)
+    }
     
 }
