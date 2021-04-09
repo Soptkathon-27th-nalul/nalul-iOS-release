e83a297dd9cfdf3ba050cc72dd78c3f4b6c3475b
xwoud <xwoud@naver.com>
2021-04-09 17:32:27 +0900

    [upload] pageController 제작

---
+++ nalul/nalul/Screen/Onboarding/ViewController/OnboardingPageController.swift
@@ -0,0 +1,29 @@
+//
+//  OnboardingPageController.swift
+//  nalul
+//
+//  Created by 김민희 on 2021/04/09.
+//
+
+import UIKit
+
+class OnboardingPageController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
