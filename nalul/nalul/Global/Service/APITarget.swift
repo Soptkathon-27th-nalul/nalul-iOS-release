2dea9b1ee27178930b7f26b204a004c8418cbd7f
xwoud <xwoud@naver.com>
2021-04-15 15:13:14 +0900

    [upload] login signup 추가
    body: 서버 통신 추가
    
    Related to: #38

--- nalul/nalul/Global/Service/APITarget.swift
+++ nalul/nalul/Global/Service/APITarget.swift
@@ -10,7 +10,8 @@ import Moya
 
 enum APITarget {
     // case 별로 api 나누기
-    case login(uuid: String) // 로그인 회원가입
+    case login(uuid: String) // 로그인
+    case signup(uuid: String) // 회원가입
     case main(jwt: String) // 9개 부위 사진 셔플받기
     case categoryFeed(category: Int, jwt: String) // 부위 카테고리 모든 게시물 받아오기
     case todayQuestion(category: Int, jwt: String) // 질문 가져오기
@@ -32,7 +33,9 @@ extension APITarget: TargetType {
         // path - 서버의 도메인 뒤에 추가 될 경로
         switch self {
         case .login:
-            return "/user"
+            return "/user/signin"
+        case .signup:
+            return "/user/signup"
         case .main:
             return "/main"
         case .categoryFeed(let category, _):
@@ -49,7 +52,7 @@ extension APITarget: TargetType {
     var method: Moya.Method {
         // method - 통신 method (get, post, put, delete ...)
         switch self {
-        case .login, .postFeed:
+        case .login, .signup, .postFeed:
             return .post
         case .main, .categoryFeed, .todayQuestion:
             return .get
@@ -69,7 +72,7 @@ extension APITarget: TargetType {
         // 파라미터 존재시에는 - .requestParameters(parameters: ["first_name": firstName, "last_name": lastName], encoding: JSONEncoding.default)
         
         switch self {
-        case .login(let uuid):
+        case .login(let uuid), .signup(let uuid):
             return .requestParameters(parameters: ["uuid" : uuid], encoding: JSONEncoding.default)
             
         case .main, .categoryFeed, .todayQuestion, .deleteFeed:
@@ -92,7 +95,7 @@ extension APITarget: TargetType {
     var headers: [String : String]? {
         // headers - HTTP header
         switch self {
-        case .login:
+        case .login, .signup:
             return ["Content-Type" : "application/json"]
         case .main(let jwt), .categoryFeed(_, let jwt), .todayQuestion(_, let jwt), .postFeed(_, _, _, let jwt), .deleteFeed(_, let jwt):
             return ["Content-Type" : "application/json", "jwt" : jwt]
