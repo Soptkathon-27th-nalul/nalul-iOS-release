fc964a2dc1e85effb4b7929cd4bca8690a937f21
xwoud <xwoud@naver.com>
2021-04-15 16:47:00 +0900

    [upload] ShuffleData 생성

---
+++ nalul/nalul/Global/Model/ShuffleData.swift
@@ -0,0 +1,12 @@
+//
+//  ShuffleData.swift
+//  nalul
+//
+//  Created by 김민희 on 2021/04/15.
+//
+
+import Foundation
+
+struct ShuffleData: Codable {
+    let random1, random2, random3, random4, random5, random6, random7, random8, random9: String
+}
