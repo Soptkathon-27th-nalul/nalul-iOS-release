1cad90521a836f1ee45ac7e40f537eb8ab9f1028
xwoud <xwoud@naver.com>
2021-04-22 23:46:26 +0900

    [upload] 약관동의뷰 생성

--- nalul/nalul/Screen/Onboarding/ViewController/OnboardingPageController.swift
+++ nalul/nalul/Screen/Onboarding/ViewController/OnboardingPageController.swift
@@ -14,6 +14,7 @@ class OnboardingPageController: UIPageViewController {
     lazy var VCArray:[UIViewController] = {
         return [self.VCInstance(name: "LogoOnboardingVC"),
                 self.VCInstance(name: "ExplainOnboardingVC"),
+                self.VCInstance(name: "PrivacyPolicyAgreementVC"),
                 self.VCInstance(name: "BackgroundSettingVC")]
     }()
     
--- nalul/nalul/Screen/Onboarding/ViewController/PrivacyPolicyAgreementVC.swift
+++ nalul/nalul/Screen/Onboarding/ViewController/PrivacyPolicyAgreementVC.swift
@@ -8,22 +8,44 @@
 import UIKit
 
 class PrivacyPolicyAgreementVC: UIViewController {
-
+    
+    // MARK: Variable Part
+    
+    // MARK: IBOutlet
+    
+    @IBOutlet weak var boxView: UIView!
+    @IBOutlet weak var agreementButton: UIButton!
+    @IBOutlet weak var agreementLabel: UILabel!
+    @IBOutlet weak var explainLabel: UILabel!
+    
+    // MARK: IBAction
+    
+    // MARK: Life Cycle Part
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setView()
         // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+// MARK: Extension
+
+extension PrivacyPolicyAgreementVC {
+    
+    // MARK: Function
+    
+    func setView() {
+        
+        self.boxView.backgroundColor = .clear
+        self.boxView.setBorder(borderColor: .white, borderWidth: 0.5)
+        
+        agreementButton.setTitle("(필수) 개인정보 수집 및 이용 방침", for: .normal)
+        agreementButton.titleLabel?.font = UIFont.threeLight(size: 16)
+        agreementButton.tintColor = .white
+        
     }
-    */
-
+    
 }
